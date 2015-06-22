b05777c40bd82a014b61533b4b4741efd6be1818
Oliver Verlinden <oliver@wps-verlinden.de>
2015-06-22 12:49:28 +0200

    Extract inner class

--- src/test/java/net/gescobar/httpserver/HttpServerTest.java
+++ src/test/java/net/gescobar/httpserver/HttpServerTest.java
@@ -123,26 +123,4 @@ public class HttpServerTest {
 		
 	}
 	
-	
-	private class TestHandler implements Handler {
-		
-		private Request request;
-
-		@Override
-		public final void handle(Request request, Response response) {
-			this.request = request;
-			
-			doHandle(request, response);
-		}
-		
-		public void doHandle(Request request, Response response) {
-			
-		}
-		
-		public Request getRequest() {
-			return request;
-		}
-		
-	}
-	
 }
